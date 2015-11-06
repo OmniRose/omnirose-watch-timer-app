e7bef6740e71a65bf05ff5b41db1f20652b17f85
Edmund von der Burg <evdb@ecclestoad.co.uk>
2015-11-06 17:56:23 +0100

    s/time/self/ and remove outer wrapping

--- www/js/services/timer.js
+++ www/js/services/timer.js
@@ -1,120 +1,116 @@
-(function() {
+angular.module('WatchTimer')
+  .factory('timer', function($rootScope, $interval, $timeout) {
+    var self = this;
 
+    self.isRunning = false;
+    self.current_state = 'stopped';
 
-  angular.module('WatchTimer')
-    .factory('timer', function($rootScope, $interval, $timeout) {
-      var timer = this;
+    self.end_time = undefined;
 
-      timer.isRunning = false;
-      timer.current_state = 'stopped';
+    // the "-0.8" is there so that after starting or restarting the timer the
+    // time changes quickly. This is to give the user a quick response that
+    // something is happening.
+    self.duration = 15 * 60;
+    self.duration -= 0.8;
 
-      timer.end_time = undefined;
+    // listed in ascending cutoff order so that the code in determine_state
+    // will work correctly.
+    self.states = [{
+      name: "alarming",
+      cutoff: -1 // FIXME -3 * 60
+    }, {
+      name: "alerting",
+      cutoff: 0
+    }, {
+      name: "notifying",
+      cutoff: 2 * 60
+    }, {
+      name: "running",
+      cutoff: 1000000000
+    }, {
+      name: "stopped",
+      cutoff: undefined
+    }, ];
 
-      // the "-0.8" is there so that after starting or restarting the timer the
-      // time changes quickly. This is to give the user a quick response that
-      // something is happening.
-      timer.duration = 15 * 60;
-      timer.duration -= 0.8;
+    self.start = function() {
+      var end_time = new Date();
+      end_time.setTime(end_time.getTime() + self.duration * 1000);
+      self.end_time = end_time;
 
-      // listed in ascending cutoff order so that the code in determine_state
-      // will work correctly.
-      timer.states = [{
-        name: "alarming",
-        cutoff: -1 // FIXME -3 * 60
-      }, {
-        name: "alerting",
-        cutoff: 0
-      }, {
-        name: "notifying",
-        cutoff: 2 * 60
-      }, {
-        name: "running",
-        cutoff: 1000000000
-      }, {
-        name: "stopped",
-        cutoff: undefined
-      }, ];
+      self.isRunning = true;
 
-      timer.start = function() {
-        var end_time = new Date();
-        end_time.setTime(end_time.getTime() + timer.duration * 1000);
-        timer.end_time = end_time;
+      // use $timeout so that this runs after we're finished so it does not
+      // conflict with our dispatch run
+      $timeout(self.monitor_changes);
+    };
 
-        timer.isRunning = true;
+    self.stop = function() {
+      self.isRunning = false;
+      $timeout(self.monitor_changes);
+    };
 
-        // use $timeout so that this runs after we're finished so it does not
-        // conflict with our dispatch run
-        $timeout(timer.monitor_changes);
-      };
+    self.change = function(amount) {
+      if (self.isRunning) {
+        var now = new Date();
+        var new_end_time = new Date();
+        new_end_time.setTime(self.end_time.getTime() + amount * 1000);
 
-      timer.stop = function() {
-        timer.isRunning = false;
-        $timeout(timer.monitor_changes);
-      };
-
-      timer.change = function(amount) {
-        if (timer.isRunning) {
-          var now = new Date();
-          var new_end_time = new Date();
-          new_end_time.setTime(timer.end_time.getTime() + amount * 1000);
-
-          if (new_end_time > now) {
-            timer.end_time = new_end_time;
-          }
-        } else {
-          var new_duration = timer.duration + amount;
-          if (new_duration > 0) {
-            timer.duration = new_duration;
-          }
+        if (new_end_time > now) {
+          self.end_time = new_end_time;
+        }
+      } else {
+        var new_duration = self.duration + amount;
+        if (new_duration > 0) {
+          self.duration = new_duration;
         }
       }
+    }
 
 
-      timer.monitor_changes = function() {
+    self.monitor_changes = function() {
 
-        timer.determine_state();
+      self.determine_state();
 
-        // Tell the scope to check for changes
-        $rootScope.$apply();
+      // Tell the scope to check for changes
+      $rootScope.$apply();
 
-        // If the timer is running then need to update later
-        if (timer.isRunning) {
-          // how long until the next update
-          var delay = Math.ceil(timer.remaining() % 1 * 1000);
-          $timeout(timer.monitor_changes, delay);
+      // If the timer is running then need to update later
+      if (self.isRunning) {
+        // how long until the next update
+        var delay = Math.ceil(self.remaining() % 1 * 1000);
+        $timeout(self.monitor_changes, delay);
+      }
+    };
+
+
+    self.determine_state = function() {
+      if (!self.isRunning) {
+        self.current_state = 'stopped';
+        return;
+      }
+
+      var remaining = self.remaining();
+      var state = _.find(
+        self.states,
+        function(state) {
+          return remaining < state.cutoff;
         }
-      };
+      );
 
-
-      timer.determine_state = function() {
-        if (!timer.isRunning) {
-          timer.current_state = 'stopped';
-          return;
-        }
-
-        var remaining = timer.remaining();
-        var state = _.find(
-          timer.states,
-          function(state) {
-            return remaining < state.cutoff;
-          }
-        );
-
-        timer.current_state = state.name;
-      };
+      self.current_state = state.name;
+    };
 
 
 
-      timer.time_to_display = function() {
-        return timer.isRunning ? timer.remaining() : timer.duration;
-      };
+    self.time_to_display = function() {
+      return self.isRunning ? self.remaining() : self.duration;
+    };
 
-      timer.remaining = function() {
-        var now = new Date();
-        return (timer.end_time - now) / 1000;
-      };
+    self.remaining = function() {
+      var now = new Date();
+      return (self.end_time - now) / 1000;
+    };
 
-      return timer;
+    return self;
 
-    });
-})();
+  });
